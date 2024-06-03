2b4113af69261e9b619cdae13dc86c86b5f3ec23
Frederico Pessoa Barbosa <143561744+Fredericobarbosa@users.noreply.github.com>
2024-06-03 00:00:42 -0300

    Add files via upload

--- JogoVue/jogo/jogo.js
+++ JogoVue/jogo/jogo.js
@@ -1,5 +1,5 @@
 const { createApp } = Vue
-const API_URL = 'https://jogo-em-vue-her-i-vil-o.onrender.com/'; //'http://localhost:3000';
+const API_URL = 'https://jogo-em-vue-her-i-vil-o.onrender.com'; //'http://localhost:3000';
 
 createApp({
     data() {
